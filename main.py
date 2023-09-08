87db6c5a5b923d7634214a70d64381bca4ed27aa
psj0919 <darkjyuk@gmail.com>
2023-09-08 19:22:38 +0900

    .

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from tqdm import tqdm
 from torchaudio.datasets import SPEECHCOMMANDS
 import os
 
+from model import M5
+
 class SubsetSC(SPEECHCOMMANDS):
     def __init__(self, subset: str = None):
         super().__init__("./", download=True)
@@ -30,6 +32,72 @@ class SubsetSC(SPEECHCOMMANDS):
             excludes = set(excludes)
             self._walker = [w for w in self._walker if w not in excludes]
 
+def train(model, epoch, log_interval):
+    losses = []
+    pbar_update = 1 / (len(train_loader) + len(test_loader))
+
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        #data = data.to(device)
+        #target = target.to(device)
+
+        # apply transform and model on whole batch directly on device
+        data = transform(data)
+        output = model(data)
+
+        # negative log-likelihood for a tensor of size (batch x 1 x n_output)
+        loss = F.nll_loss(output.squeeze(), target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # print training stats
+        if batch_idx % log_interval == 0:
+            print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}")
+
+        # update progress bar
+        pbar.update(pbar_update)
+        # record loss
+        losses.append(loss.item())
+
+    plt.plot(losses)
+    plt.title("training loss")
+
+def eval(model, epoch):
+    pbar_update = 1 / (len(train_loader) + len(test_loader))
+    model.eval()
+    correct = 0
+    for data, target in test_loader:
+        # data = data.to(device)
+        # target = target.to(device)
+
+        # apply transform and model on whole batch directly on device
+        data = transform(data)
+        output = model(data)
+
+        pred = get_likely_index(output)
+        correct += number_of_correct(pred, target)
+
+        # update progress bar
+        pbar.update(pbar_update)
+    print(f"\nTest Epoch: {epoch}\tAccuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset):.0f}%)\n")
+
+def predict(tensor):
+    # Use the model to predict the label of the waveform
+    # tensor = tensor.to(device)
+    tensor = transform(tensor)
+    tensor = model(tensor.unsqueeze(0))
+    tensor = get_likely_index(tensor)
+    tensor = index_to_label(tensor.squeeze())
+    return tensor
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 def label_to_index(word):
     # Return the position of the word in labels
     return torch.tensor(labels.index(word))
@@ -40,13 +108,44 @@ def index_to_label(index):
     # This is the inverse of label_to_index
     return labels[index]
 
-def resample_rate(rate, n_rate):
+def pad_sequence(batch):
+    # Make all tensor in a batch the same length by padding with zeros
+    batch = [item.t() for item in batch]
+    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
+    return batch.permute(0, 2, 1)
 
-    transform = torchaudio.transforms.Resample(orig_freq=rate, new_freq=n_rate)
-    transformed = transform(waveform)
-    return transformed
+def collate_fn(batch):
+
+    # A data tuple has the form:
+    # waveform, sample_rate, label, speaker_id, utterance_number
+
+    tensors, targets = [], []
+
+    # Gather in lists, and encode labels as indices
+    for waveform, _, label, *_ in batch:
+        tensors += [waveform]
+        targets += [label_to_index(label)]
+
+    # Group the list of tensors into a batched tensor
+    tensors = pad_sequence(tensors)
+    targets = torch.stack(targets)
+
+    return tensors, targets
+
+def number_of_correct(pred, target):
+    # count number of correct predictions
+    return pred.squeeze().eq(target).sum().item()
+
+
+def get_likely_index(tensor):
+    # find most likely label index for each element in the batch
+    return tensor.argmax(dim=-1)
+
+
 
 if __name__=='__main__':
+    global new_sample_rate
+
     # --------------------GPU 또는 CPU 사용-------------------------
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -74,18 +173,84 @@ if __name__=='__main__':
 
     waveform_last, *_ = train_set[-1]
     ipd.Audio(waveform_last.numpy(), rate=sample_rate)
-    # --------------------------------------------------------------------
+    # ---------------------------------------------------------------------
 
 
     # --------------------- sample_rate를 16KHz에서 8KHz로 변경 ----------------------------------
-    transformed = resample_rate(16000,8000)
+    new_sample_rate = 8000  # 8KHz로 사용
+    transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
+    transformed = transform(waveform)
     plt.plot(transformed.t().numpy())
-    ipd.Audio(transformed.numpy(), rate=8000)
-    # --------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
 
-    # --------------------
-    word_start = "yes"
-    index = label_to_index(word_start)
-    word_recovered = index_to_label(index)
+    # -------------------
+    batch_size = 256
+    if device == "cuda":
+        num_workers = 1
+        pin_memory = True
+    else:
+        num_workers = 0
+        pin_memory = False
+    # ------------------------------------------------------
 
-    print(word_start, "-->", index, "-->", word_recovered)
+    # ------------------data_load-------------------------
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        collate_fn=collate_fn,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+    )
+    # -----------------------------------------------------
+
+    # ---------------------- model ----------------------------
+    model = M5(n_input=transformed.shape[0], n_output=len(labels))
+    #model.to(device)
+    print(model)
+    # -------------------------------------------------------------
+
+    # ----------------optimizer, scheduler setup--------------------------------
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-5)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+    # ---------------------------------------------------------------------------
+
+
+    #-----------------training-------------------------
+    log_interval = 20
+    n_epoch = 10
+    transform = transform.to(device)
+    with tqdm(total=n_epoch) as pbar:
+        for epoch in range(1, n_epoch + 1):
+            train(model, epoch, log_interval)
+            eval(model, epoch)
+            scheduler.step()
+    # ----------------------------------------------------
+
+    # ----------------predict----------------------------------------
+    waveform, sample_rate, utterance, *_ = train_set[-1]
+    print(f"Expected: {utterance}. Predicted: {predict(waveform)}.")
+
+
+    for i, (waveform, sample_rate, utterance, *_) in enumerate(test_set):
+        output = predict(waveform)
+        if output != utterance:
+            print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
+            break
+
+        else:
+            print("All examples in this dataset were correctly classified!")
+            print("In this case, let's just look at the last data point")
+            print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
+
+    # ----------------------------------------------------------------
